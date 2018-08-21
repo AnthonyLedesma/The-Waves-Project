18926a4d1960e40a6ae18bfe18d060a9387c1aed
paranoia1906 <anthonymledesma@gmail.com>
2018-08-21 07:24:34 -0700

    Added bcrypt password hashing.

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+const bcrypt = require('bcrypt');
+const SALT_I = 10;
+
 const userSchema = mongoose.Schema({
     email:{
         type:String,
@@ -39,6 +42,21 @@ const userSchema = mongoose.Schema({
         type:String,
     }
 });
+
+userSchema.pre('save',function(next){
+    var user = this;
+
+    bcrypt.genSalt(SALT_I,function(err,salt){
+        if(err) return next(err);
+        
+        bcrypt.hash(user.password,salt,function(err,hash){
+            if(err) return next(err);
+            user.password = hash;
+            next();
+        });
+        
+    })
+})
 
 const User = mongoose.model('User',userSchema);
 
