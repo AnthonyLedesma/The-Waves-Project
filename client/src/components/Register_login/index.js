78179ff11ddbc99c2d5d4c39cff0ab4bbb203ebc
paranoia1906 <anthonymledesma@gmail.com>
2018-08-22 12:16:49 -0700

    Created route for /register_login. Added RegisterLogin module to components. Started creation of reusable button component MyButton.

---
+++ client/src/components/Register_login/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import MyButton from '../utils/button';
+
+const RegisterLogin = () => {
+    return (
+        <div className="page_wrapper">
+            <div className="container">
+                <div className="register_login_container">
+                    <div className="left">
+                        <h1>New Customers</h1>
+                        <p>In auctor, justo eget aliquet interdum, elit lorem facilisis enim, in scelerisque dui leo sit amet mauris. Mauris ut erat vitae mi dignissim dignissim quis vel diam. Phasellus at eros dolor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce in urna eu magna imperdiet volutpat. Nunc nisl nisi, mollis ut diam eu, efficitur varius dui. Nullam ut justo sit amet nibh aliquet placerat nec non nisi.</p>
+                    </div>
+                    <div className="right">
+                        <h1>Registered Customers</h1>
+                        <p>If you have an account please log in</p>
+                    </div>
+                </div>
+            </div>
+            REGISTER
+        </div>
+    );
+};
+
+export default RegisterLogin;
---
+++ client/src/components/utils/button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const MyButton = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MyButton;
