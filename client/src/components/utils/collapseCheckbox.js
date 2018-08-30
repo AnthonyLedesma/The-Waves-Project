385804c26ac13b93d9949818a503d3f65bb5bac0
paranoia1906 <anthonymledesma@gmail.com>
2018-08-30 12:05:02 -0700

    Started list of product categores display. Using Material UI collapse element with checkboxes to filter categories. Next have to pull information from state.

--- client/src/components/utils/collapseCheckbox.js
+++ client/src/components/utils/collapseCheckbox.js
@@ -20,12 +20,59 @@ class CollapseCheckbox extends Component {
         checked: []
     }
 
+    componentDidMount() {
+        if (this.props.initState) {
+            this.setState({
+                open: this.props.initState
+            })
+        }
+    }
 
+    handleClick() {
+        this.setState({ open: !this.state.open })
+    }
+
+    handleAngle = () => (
+        this.state.open ?
+            <FontAwesomeIcon
+                icon={faAngleUp}
+                className="icon"
+            />
+            :
+            <FontAwesomeIcon
+                icon={faAngleDown}
+                className="icon"
+            />
+    )
+
+    renderList = () => (
+        this.props.list ?
+            this.props.list.map((value,i)=>(
+                <ListItem>
+                    
+                </ListItem>
+            ))
+        :null
+    )
 
     render() {
         return (
-            <div>
-                
+            <div className="collapse_items_wrapper">
+                <List style={{ borderBottom: '1px solid #dbdbdb' }}>
+                    <ListItem onClick={this.handleClick} style={{ padding: '10px 23px 10px 0' }}>
+                        <ListItemText
+                            primary={this.props.title}
+                            className="collapse_title"
+                        />
+                        {this.handleAngle()}
+                    </ListItem>
+                    <Collapse in={this.state.open} timeout="auto" unmountOnExit>
+                        <List component="div" disablePadding>
+                            {this.renderList()}
+                        </List>
+                    </Collapse>
+
+                </List>
             </div>
         );
     }
