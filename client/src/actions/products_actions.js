b0a38692e560559fdd0600ed3e498c1e7747a279
paranoia1906 <anthonymledesma@gmail.com>
2018-08-30 09:59:14 -0700

    Created PageTop reusable component for shop/product pages. Created requests to getBrands and getWoods for product categories. Added route for /shop.

--- client/src/actions/products_actions.js
+++ client/src/actions/products_actions.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
 import {
     GET_PRODUCTS_BY_SELL,
-    GET_PRODUCTS_BY_ARRIVAL
+    GET_PRODUCTS_BY_ARRIVAL,
+    GET_BRANDS,
+    GET_WOODS
 } from './types';
 
 import { PRODUCT_SERVER } from '../components/utils/misc';
+
+//============================================
+//                  PROMO AREA
+//============================================
 
 export function getProductsByArrival() {
     // /articles?sortBy=createdAt&order=desc&limit=4
@@ -22,6 +28,30 @@ export function getProductsBySell() {
         .then(response => response.data)
     return {
         type: GET_PRODUCTS_BY_SELL,
+        payload: request
+    }
+}
+
+//============================================
+//                  CATEGORIES
+//============================================
+
+export function getBrands() {
+    const request = axios.get(`${PRODUCT_SERVER}/brands`)
+        .then(response => response.data)
+    return {
+        type: GET_BRANDS,
+        payload: request
+    }
+}
+
+
+
+export function getWoods() {
+    const request = axios.get(`${PRODUCT_SERVER}/woods`)
+        .then(response => response.data)
+    return {
+        type: GET_WOODS,
         payload: request
     }
 }
