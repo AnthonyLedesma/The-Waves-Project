a5b659d23fa1e1a81b208e2dcc507766fa65a722
paranoia1906 <anthonymledesma@gmail.com>
2018-08-30 08:46:46 -0700

    Added cards to the homepage. Card component uses CardBlock for layout and itterator.

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import HomeSlider from './home_slider';
 import HomePromotion from './home_promotion';
+import CardBlock from '../utils/card_block';
 
 import { connect } from 'react-redux';
 import { getProductsBySell, getProductsByArrival } from '../../actions/products_actions';
 
 class Home extends Component {
 
-    componentDidMount(){
+    componentDidMount() {
         this.props.dispatch(getProductsBySell());
         this.props.dispatch(getProductsByArrival());
     }
@@ -16,7 +17,13 @@ class Home extends Component {
         return (
             <div>
                 <HomeSlider />
+                <CardBlock
+                    list={this.props.products.bySell}
+                    title="Best Selling Guitars" />
                 <HomePromotion />
+                <CardBlock
+                    list={this.props.products.byArrival}
+                    title="The Latest Arrivals" />
             </div>
         );
     }
