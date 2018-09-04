4884145c1581faac817ca0b4f93e54486b8b338d
paranoia1906 <anthonymledesma@gmail.com>
2018-09-04 09:33:47 -0700

    Started to add fields to /admin/add_products page. AddProduct componenet state setup.

--- client/src/components/utils/forms/formFields.js
+++ client/src/components/utils/forms/formFields.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const FormField = ({formdata,change,id}) => {
+const FormField = ({ formdata, change, id }) => {
 
-    const showError = () =>{
+    const showError = () => {
         let errorMessage = null;
-        if(formdata.validation && !formdata.valid){
+        if (formdata.validation && !formdata.valid) {
             errorMessage = (
                 <div className="error_label">
                     {formdata.validationMessage}
@@ -14,25 +14,31 @@ const FormField = ({formdata,change,id}) => {
         return errorMessage;
     }
 
-    const  renderTemplate = () => {
+    const renderTemplate = () => {
         let formTemplate = null;
-        switch(formdata.element){
-            case('input'):
+        switch (formdata.element) {
+            case ('input'):
                 formTemplate = (
                     <div className="formBlock">
+                        {formdata.showlabel ? 
+                         <div className="label_inputs">
+                            {formdata.config.label}
+                         </div>
+                    : null}
+
                         <input
                             {...formdata.config}
                             value={formdata.value}
-                            onBlur={(event)=> change({event,id,blur:true})}
-                            onChange={(event)=> change({event,id})}
+                            onBlur={(event) => change({ event, id, blur: true })}
+                            onChange={(event) => change({ event, id })}
                         />
                         {showError()}
                     </div>
                 )
-            break;
+                break;
             default:
                 formTemplate = '';
-            break;
+                break;
         }
         return formTemplate;
     }
