ab3ea4d4e4c46466919f081735b9f32e1017d6fa
paranoia1906 <anthonymledesma@gmail.com>
2018-09-04 08:09:07 -0700

    Added grid view to /shop page.

--- client/src/components/utils/card.js
+++ client/src/components/utils/card.js
@@ -21,34 +21,37 @@ class Card extends Component {
                     <div className="tags">
                         <div className="brand">{props.brand.name}</div>
                         <div className="name">{props.name}</div>
-                        <div className="price">{props.price}</div>
+                        <div className="price">${props.price}</div>
                     </div>
 
-                </div>
-                {props.grid ?
-                    <div className="description">
-                        "description"
+
+                    {props.grid ?
+                        <div className="description">
+                            <p>
+                                {props.description}
+                            </p>
                     </div>
-                    : null}
-                <div className="actions">
-                    <div className="button_wrap">
-                        <MyButton
-                            type="default"
-                            altClass="card_link"
-                            title="View Product"
-                            linkTo={`/product_detail/${props._id}`}
-                            addStyles={{
-                                margin: '10px 0 0 0'
-                            }}
-                        />
-                    </div>
-                    <div className="button_wrap">
-                        <MyButton
-                            type="bag_link"
-                            runAction={()=>{
-                                console.log('added to cart')
-                            }}
-                        />
+                        : null}
+                    <div className="actions">
+                        <div className="button_wrap">
+                            <MyButton
+                                type="default"
+                                altClass="card_link"
+                                title="View Product"
+                                linkTo={`/product_detail/${props._id}`}
+                                addStyles={{
+                                    margin: '10px 0 0 0'
+                                }}
+                            />
+                        </div>
+                        <div className="button_wrap">
+                            <MyButton
+                                type="bag_link"
+                                runAction={() => {
+                                    console.log('added to cart')
+                                }}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
