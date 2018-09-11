be0ca76b59104f65169c298e2ff1a1718be76b08
paranoia1906 <anthonymledesma@gmail.com>
2018-09-11 10:54:43 -0700

    User action removeCartItem is created. Route and reducers are setup to properly remove selected items from cart.

--- client/src/components/User/cart.js
+++ client/src/components/User/cart.js
@@ -3,7 +3,7 @@ import UserLayout from '../../hoc/user';
 import UserProductBlock from '../utils/User/product_block';
 
 import { connect } from 'react-redux';
-import { getCartItems } from '../../actions/user_actions';
+import { getCartItems, removeCartItem } from '../../actions/user_actions';
 
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import faFrown from '@fortawesome/fontawesome-free-solid/faFrown';
@@ -36,6 +36,19 @@ class UserCart extends Component {
             }
         }
 
+    }
+
+    removeFromCart = (id) => {
+        this.props.dispatch(removeCartItem(id))
+        .then(()=>{
+            if(this.props.user.cartDetail.length <= 0){
+                this.setState({
+                    showTotal: false
+                })
+            } else {
+                this.calculateTotal(this.props.user.cartDetail);
+            }
+        })
     }
 
     calculateTotal = (cartDetail) => {
--- client/src/reducers/user_reducer.js
+++ client/src/reducers/user_reducer.js
@@ -1,4 +1,4 @@
-import { LOGIN_USER, LOGOUT_USER, REGISTER_USER, AUTH_USER, ADD_TO_CART_USER, GET_CART_ITEMS_USER } from '../actions/types';
+import { LOGIN_USER, LOGOUT_USER, REGISTER_USER, AUTH_USER, ADD_TO_CART_USER, GET_CART_ITEMS_USER, REMOVE_CART_ITEMS_USER } from '../actions/types';
 
 export default function (state = {}, action) {
     switch (action.type) {
@@ -21,6 +21,15 @@ export default function (state = {}, action) {
             return {
                 ...state,
                 cartDetail: action.payload
+            }
+        case REMOVE_CART_ITEMS_USER:
+            return {
+                ...state,
+                cartDetail: action.payload.cartDetail,
+                userData: {
+                    ...state.userData,
+                    cart:action.payload.cart
+                }
             }
         default:
             return state;
